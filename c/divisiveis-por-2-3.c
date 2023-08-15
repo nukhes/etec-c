6e13f5a870ebe774a37ec2587345c6ddeef4ff44
nukhes <nukhes@protonmail.com>
2023-08-14 21:40:15 -0300

    aaaaaaaaaaa secretarias

---
+++ c/divisiveis-por-2-3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c, d;
+
+	printf("digite um numero: ");
+	scanf("%i", &a);
+	printf("digite um numero: ");
+	scanf("%i", &b);
+	printf("digite um numero: ");
+	scanf("%i", &c);
+	printf("digite um numero: ");
+	scanf("%i", &d);
+
+	if ((a % 2) ==  0 && (a % 3) == 0) {
+		printf("%i e divisivel por 2 e 3.", a)
+	}
+	if ((b % 2) ==  0 && (a % 3) == 0) {
+		printf("%i e divisivel por 2 e 3.", b)
+	}
+	if ((c % 2) ==  0 && (a % 3) == 0) {
+		printf("%i e divisivel por 2 e 3.", c)
+	}
+	if ((d % 2) ==  0 && (a % 3) == 0) {
+		printf("%i e divisivel por 2 e 3.", d)
+	}
+}
--- c/media-aprimorada.c
+++ c/media-aprimorada.c
@@ -10,30 +10,30 @@ logico      -   bool (%b)
 */
 
 int main() {
-    int ix;
-    float c,d;
-    float nums[4][1];
-    
-    for (ix = 1; ix < 5; ix++) {
-        printf("DIGITE UMA NOTA: ");
-        scanf("%f", &nums[ix][1]);
-    }
-    
-    c = (nums[1][1] + nums[2][1] + nums[3][1] + nums[4][1])/4;
-    
-    if (c >= 7) {
-        printf("APROVADO");
-    } else {
-        printf("DIGITE A NOTA EXAME: ");
-        scanf("%f", &d);
-        c = (c+d)/2;
-        if (c >= 5) {
-            printf("APROVADO EM EXAME");
-        } else {
-            printf("REPROVADO");
-        }
-    }  
-    printf(", SUA MEDIA E: %.2f\n\n", c);
+	int ix;
+	float c,d;
+	float nums[4][1];
 
-    return 0;
+	for (ix = 1; ix < 5; ix++) {
+		printf("DIGITE UMA NOTA: ");
+		scanf("%f", &nums[ix][1]);
+	}
+
+	c = (nums[1][1] + nums[2][1] + nums[3][1] + nums[4][1])/4;
+
+	if (c >= 7) {
+		printf("APROVADO");
+	} else {
+		printf("DIGITE A NOTA EXAME: ");
+		scanf("%f", &d);
+		c = (c+d)/2;
+		if (c >= 5) {
+			printf("APROVADO EM EXAME");
+		} else {
+			printf("REPROVADO");
+		}
+	}  
+	printf(", SUA MEDIA E: %.2f\n\n", c);
+
+	return 0;
 }
